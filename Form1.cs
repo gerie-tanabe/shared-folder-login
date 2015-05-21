24ba4e3ed867f51e54968a0ac3ee14e15062aa5f
Gerardo B. Tanabe <gbtanabe@gmail.com>
2015-05-21 09:02:18 +0800

    init

---
+++ Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Net;
+
+namespace Shared_Folder_Login
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+           
+            string sharedFolderPath = txtSharedFolder.Text;
+            string username = txtUsername.Text;
+            string password = txtPassword.Text;
+
+            NetworkCredential readCredentials = new NetworkCredential(username, password);
+            using (new NetworkConnection(sharedFolderPath, readCredentials))           
+            {
+                
+                MessageBox.Show(Directory.Exists(sharedFolderPath).ToString());
+File.Copy("sample.txt",sharedFolderPath + "\\sample.txt");
+            }
+        }
+    }
+}
